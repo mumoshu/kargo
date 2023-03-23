46b2b12681a144850a2d506b5c04ef89f673ed00
Yusuke Kuoka <ykuoka@gmail.com>
2023-03-23 11:29:55 +0000

    Make compose test DRY

--- generator_test.go
+++ generator_test.go
@@ -8,26 +8,36 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerate_ComposeApply(t *testing.T) {
-	g := &kargo.Generator{
-		GetValue: func(key string) (string, error) {
-			return strings.ToUpper(key), nil
-		},
-		TailLogs: false,
-	}
+func TestGenerate_Compose(t *testing.T) {
+	run := func(t *testing.T, targ kargo.Target, f func(fg *kargo.Generator, fc *kargo.Config), expected []kargo.Cmd) {
+		t.Helper()
 
-	c := &kargo.Config{
-		Name:    "test",
-		Path:    "testdata/compose",
-		Compose: &kargo.Compose{},
-	}
+		g := &kargo.Generator{
+			GetValue: func(key string) (string, error) {
+				return strings.ToUpper(key), nil
+			},
+			TailLogs: false,
+		}
 
-	t.Run("apply", func(t *testing.T) {
-		cmds, err := g.ExecCmds(c, kargo.Apply)
+		c := &kargo.Config{
+			Name:    "test",
+			Path:    "testdata/compose",
+			Compose: &kargo.Compose{},
+		}
+
+		f(g, c)
+
+		cmds, err := g.ExecCmds(c, targ)
 
 		require.NoError(t, err)
 
-		require.Equal(t, []kargo.Cmd{
+		require.Equal(t, expected, cmds)
+	}
+
+	t.Run("apply", func(t *testing.T) {
+		run(t, kargo.Apply, func(g *kargo.Generator, c *kargo.Config) {
+			g.TailLogs = false
+		}, []kargo.Cmd{
 			{
 				Name: "docker",
 				Args: []string{
@@ -39,15 +49,13 @@ func TestGenerate_ComposeApply(t *testing.T) {
 				},
 				Dir: "testdata/compose",
 			},
-		}, cmds)
+		})
 	})
 
 	t.Run("plan", func(t *testing.T) {
-		cmds, err := g.ExecCmds(c, kargo.Plan)
-
-		require.NoError(t, err)
-
-		require.Equal(t, []kargo.Cmd{
+		run(t, kargo.Plan, func(g *kargo.Generator, c *kargo.Config) {
+			g.TailLogs = false
+		}, []kargo.Cmd{
 			{
 				Name: "docker",
 				Args: []string{
@@ -58,6 +66,6 @@ func TestGenerate_ComposeApply(t *testing.T) {
 				},
 				Dir: "testdata/compose",
 			},
-		}, cmds)
+		})
 	})
 }

5f8bfdc02b51cc75866d8c17cb9fd33c25eb0ad9
Yusuke Kuoka <ykuoka@gmail.com>
2024-01-30 01:47:43 +0000

    Add support for empty git user.email

--- generator_gitops.go
+++ generator_gitops.go
@@ -130,6 +130,12 @@ func (g *Generator) gitOps(t Target, name, repo, branch, head, path string, copi
 		gitCommitArgs = gitCommitArgs.Append(
 			"git", "config", "user.email", prOpts.GitUserEmail, ";",
 		)
+	} else {
+		// We need to set user.email to an empty string anyway to avoid:
+		//   fatal: unable to auto-detect email address (got 'mylinuxuser@myhostname.(none)')
+		gitCommitArgs = gitCommitArgs.Append(
+			"(", "git", "config", "user.email", "||", "git", "config", "user.email", "", ")", ";",
+		)
 	}
 	gitCommitArgs = gitCommitArgs.Append(
 		"git", "commit", "-m", "'automated commit'",
